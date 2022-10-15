88921b7a7d06630306db542112471c3f40949c3d
tfacal <tomas.facal@udc.es>
2022-10-15 14:00:06 +0200

    modified spider and results

--- receipes_founder/spiders/receipesSpider.py
+++ receipes_founder/spiders/receipesSpider.py
@@ -1,27 +1,20 @@
 import scrapy
- 
+
+
 class ReceipesSpider(scrapy.Spider):
     name = 'receipes'
-    allowed_domains = ['http://recetasgratis.net/',]
-    start_urls = ['http://recetasgratis.net/',]
+    allowed_domains = ['http://recetasgratis.net/', 'recetasgratis.net']
+    start_urls = ['http://recetasgratis.net/', ]
 
-    def start_request(self, response):
-      # urls = response.css('div.categoria a::attr(href)').extract()
-      # for url in urls:
-      url = 'http://recetasgratis.net/'
-      yield scrapy.Request(url , callback=self.parse)
-
-    # def parse_receipes(self, response):
-    #     for info in response.css('div'):
-    #         yield {
-    #             'product_name': info.css('h2::text').extract_first(),
-    #         }
+    def parse_link(self, response):
+        for link in response.css('div.resultado'):
+            yield {"nombre": link.css('a::text').extract(),
+                   "comensales": link.css('div.properties span.comensales::text').extract(),
+                   "duracion": link.css('div.properties span.duracion::text').extract(),
+                   "para": link.css('div.properties span.para::text').extract(),
+                   "dificultad": link.css('div.info_snippet span::text').extract()
+                   }
 
     def parse(self, response):
-      # content = response.css('div').extract()
-      # yield {'content':content}
-      for link in response.css('div.categoria a::attr(href)').extract():
-        yield {'content': link}
-
-
-         
+        for link in response.css('div.categoria a::attr(href)').extract():
+            yield scrapy.Request(link, callback=self.parse_link)
