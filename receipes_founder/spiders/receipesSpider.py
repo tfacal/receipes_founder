75f50299becc11d02dcd0d6306c8db568713bd07
tfacal <tomas.facal@udc.es>
2022-10-15 15:25:22 +0200

    crawled url

--- receipes_founder/spiders/receipesSpider.py
+++ receipes_founder/spiders/receipesSpider.py
@@ -7,12 +7,17 @@ class ReceipesSpider(scrapy.Spider):
     start_urls = ['http://recetasgratis.net/', ]
 
     def parse_link(self, response):
-        for link in response.css('div.resultado'):
-            yield {"nombre": link.css('a::text').extract(),
+        for link in response.css('div.resultado a::attr(href)').extract():
+            yield scrapy.Request(link, callback=self.parse_link2)
+
+    def parse_link2(self, response):
+        for link in response.css('div.header-gap'):
+            yield {"nombre": link.css('article h1::text').extract(),
                    "comensales": link.css('div.properties span.comensales::text').extract(),
                    "duracion": link.css('div.properties span.duracion::text').extract(),
                    "para": link.css('div.properties span.para::text').extract(),
-                   "dificultad": link.css('div.info_snippet span::text').extract()
+                   "dificultad": link.css('div.properties span.dificultad::text').extract(),
+                   "ingredientes": link.css('div.ingredientes li.ingrediente label::text').extract()
                    }
 
     def parse(self, response):
