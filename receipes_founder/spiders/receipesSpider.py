1c81bc2904a14a5dd4e562830f760c94011fe2e1
tfacal <72490967+tfacal@users.noreply.github.com>
2022-11-17 20:24:01 +0100

    Modified duration (#5)
    * added solr
    
    * create dataset
    
    * solr working
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * added comands file
    
    * fix: Añadios .pyc en gitignore
    
    * fix: gitignore
    
    * feat: Obtener pasos de la receta
    
    * modified duration
    
    * deleted unused file
    
    Co-authored-by: Clara Juncal <clara.juncal.ESP@qorbital.com>
    Co-authored-by: clarajuncal1 <74610088+clarajuncal1@users.noreply.github.com>
    Co-authored-by: Martín Caamaño Rodríguez <mcaamano@enxenio.es>

--- receipes_founder/spiders/receipesSpider.py
+++ receipes_founder/spiders/receipesSpider.py
@@ -1,5 +1,7 @@
+from collections import defaultdict
 import scrapy
-
+from collections import defaultdict
+import re
 
 class ReceipesSpider(scrapy.Spider):
     name = 'receipes'
@@ -9,17 +11,39 @@ class ReceipesSpider(scrapy.Spider):
     def parse_link(self, response):
         for link in response.css('div.resultado a::attr(href)').extract():
             yield scrapy.Request(link, callback=self.parse_link2)
+    def parse_duration(self, s):
+        d = {
+            'd':      24*60,
+            'day':    24*60,
+            'days':   24*60,
+            'h':      60,
+            'hr':     60,
+            'hour':   60,
+            'hours':  60,
+        }
+        mult_items = defaultdict(lambda: 1).copy()
+        mult_items.update(d)
+
+        parts = re.search(r'^(\d+)([^\d]*)', s.lower().replace(' ', ''))
+        if parts:
+            return int(parts.group(1)) * mult_items[parts.group(2)] + self.parse_duration(re.sub(r'^(\d+)([^\d]*)', '', s.lower()))
+        else:
+            return 0
 
     def parse_link2(self, response):
         for link in response.css('div.header-gap'):
             yield {"nombre": link.css('article h1::text').extract(),
                    "comensales": link.css('div.properties span.comensales::text').extract(),
-                   "duracion": link.css('div.properties span.duracion::text').extract(),
+                   "duracion": self.parse_duration(str(link.css('div.properties span.duracion::text').extract()[0]).strip()),
                    "para": link.css('div.properties span.para::text').extract(),
                    "dificultad": link.css('div.properties span.dificultad::text').extract(),
-                   "ingredientes": link.css('div.ingredientes li.ingrediente label::text').extract()
-                   }
+                   "ingredientes": link.css('div.ingredientes li.ingrediente label::text').extract(),
+                   "apartados": link.css('div.apartado p::text').extract()
+                   } 
+            print(str(link.css('div.properties span.duracion::text').extract()[0]).strip())    
 
     def parse(self, response):
         for link in response.css('div.categoria a::attr(href)').extract():
             yield scrapy.Request(link, callback=self.parse_link)
+
+    
